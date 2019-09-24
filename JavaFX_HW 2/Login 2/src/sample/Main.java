cffd60882e4818bc8c876527c710e8392d187bc5
throppa <42742571+throppa@users.noreply.github.com>
2019-09-24 14:13:55 -0400

    Update Main.java

--- JavaFX_HW 2/Login 2/src/sample/Main.java
+++ JavaFX_HW 2/Login 2/src/sample/Main.java
@@ -1,9 +1,18 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
