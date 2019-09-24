273c65897d47080541b3d786f74f59786c88efc0
throppa <42742571+throppa@users.noreply.github.com>
2019-09-24 14:13:07 -0400

    Update Main.java

--- JavaFX_HW 2/Login/src/sample/Main.java
+++ JavaFX_HW 2/Login/src/sample/Main.java
@@ -1,11 +1,12 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 public class Main extends Application {
 
     /*@Override
